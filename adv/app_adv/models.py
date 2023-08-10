466e34e2f503a3e923a70e64bab204f49ac4cecd
kartofanushka <angelaseliv@gmail.com>
2023-08-11 01:26:02 +0700

    l8

--- adv/app_adv/models.py
+++ adv/app_adv/models.py
@@ -1,5 +1,5 @@
 from django.db import models, migrations
-
+from django.contrib import admin
 
 # Create your models here.
 class Advertisement(models.Model):
@@ -14,6 +14,26 @@ class Advertisement(models.Model):
         db_table="advertisements"
     
     def __str__(self):
-        return f"id={self.id}, title={self.title},  price={self.price}"
+        return f"Advertisement:(id={self.id}, title={self.title},  price={self.price})"
     #Advertisement: Advertisement(id=1, title=Заголовок №1, price=100.00)
 
+    # template "Today 12:34:36"
+    @admin.display(description=  'Creation date')
+    def cteated_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time= self.created_at.time().strftime("%H:%M:%S")
+            return "Today ", created_time
+
+        # print ("self: ",self.created_at.date(),"now", timezone.now().date())
+        return self.created_at.strftime("%d/%m/%Y, %H:%M:%S")
+   
+    @admin.display(description = 'Updated date')
+    def updated_date(self):
+        from django.utils import timezone, html
+        if self.updated_at.date() == timezone.now().date():
+            updated_time= self.updated_at.time().strftime("%H:%M:%S")
+            return html.format_html(
+                "<span style='color:grey'>Today {}</span>", updated_time
+            )
+        return self.updated_at.strftime("%d/%m/%Y, %H:%M:%S")
