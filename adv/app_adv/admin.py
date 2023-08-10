43354267533628d90fdf12847b9fbd446c3d276b
kartofanushka <140699782+kartofanushka@users.noreply.github.com>
2023-08-10 21:29:15 +0300

    Update admin.py

--- adv/app_adv/admin.py
+++ adv/app_adv/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Advertisement
 
 # Register your models here.
+
+class Adv_admin(admin.ModelAdmin):
+    list_display = ["id","auction", "title", "description", "price", "cteated_date", "updated_date"]
+    list_display_links = [ "title"]
+    list_filter = ["updated_at", "title"]
+    list_per_page = 50
+    actions = ["make_auctions_as_false", "make_auctions_as_true" ] # need function to work 
+    fieldsets = (
+        ('Main', {
+            "fields":("title", "description")}),
+        ('Finance', {
+            "fields":("price", "auction"),
+            #"collapse":["collapse"] # doesn't work Fieldset.__init__() got an unexpected keyword argument 'collapse'
+        }))
+    # function name same as in action
+    @admin.action(description="Disable auction option")
+    def make_auctions_as_false(self, request, queryset):
+        queryset.update(auction=False)
+    
+    @admin.action(description="Enable auction option")
+    def make_auctions_as_true(self, request, queryset):
+        queryset.update(auction=True)
+    
+
+    
+admin.site.register(Advertisement, Adv_admin)
